86b757ac060ea0147eabf0ed5f41268bf8742e0a
boh001 <boh001g@gmail.com>
2020-04-29 09:43:48 +0900

    Updates

---
+++ precache-manifest.9293612ed7f6de1c2c229dd9f1c3fac7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "540bcdc014fadf8546048524c7f7302c",
+    "url": "/kimflix/index.html"
+  },
+  {
+    "revision": "1b091db9699d0ddeeb27",
+    "url": "/kimflix/static/js/2.a4e26d80.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kimflix/static/js/2.a4e26d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47fdad877ee89e37b385",
+    "url": "/kimflix/static/js/main.efe7deac.chunk.js"
+  },
+  {
+    "revision": "7a2dfc83cbcde9561a86",
+    "url": "/kimflix/static/js/runtime-main.e062ca24.js"
+  },
+  {
+    "revision": "a0ae82f2898e0c60f9d1dddc1dd3cf3d",
+    "url": "/kimflix/static/media/test.a0ae82f2.mp4"
+  }
+]);
