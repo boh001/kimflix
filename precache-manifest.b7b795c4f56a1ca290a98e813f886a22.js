2d0745445362a27c44a9445da5471c253d22c03c
boh001 <boh001g@gmail.com>
2020-04-29 09:42:10 +0900

    Updates

---
+++ precache-manifest.b7b795c4f56a1ca290a98e813f886a22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b166c9aea510c004f9857f6c29ef8942",
+    "url": "/index.html"
+  },
+  {
+    "revision": "270a0fcd57123241ede6",
+    "url": "/static/js/2.15b2d7e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.15b2d7e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bfd3851ba600533a58a",
+    "url": "/static/js/main.33c48837.chunk.js"
+  },
+  {
+    "revision": "06972fab05a77df0e042",
+    "url": "/static/js/runtime-main.db1de549.js"
+  },
+  {
+    "revision": "a0ae82f2898e0c60f9d1dddc1dd3cf3d",
+    "url": "/static/media/test.a0ae82f2.mp4"
+  }
+]);
